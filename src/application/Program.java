71cd0329380b3ead97087ff18e5bffe4f4010cc6
Sammy Richard <simonesammy9@gmail.com>
2025-03-03 19:09:33 -0300

    add Delete Implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();	
 		
 		System.out.println("=== TEST 1: seller findById ===");
@@ -43,6 +46,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Updated completed!");
+		
+		System.out.println("\n=== TEST 6: seller DELETE ===");
+		System.out.print("Enter ID for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delet completed!");
+
+		sc.close();
 	}
 	
 	
